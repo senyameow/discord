76893b19c6b7b574a209174d0fb6422f8f0f3ac4
senyameow <arseniyxd@gmail.com>
2023-09-05 21:09:43 +0700

    make a channel page (to do)

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,6 +1,38 @@
+import { currentProfile } from '@/lib/current-profile';
+import { db } from '@/lib/db';
+import { redirectToSignIn } from '@clerk/nextjs';
 import React from 'react'
 
-const ChannelIdPage = () => {
+interface ChannelIdPageProps { // если у нас вложенные друг в друга (даже через одного) руты в скобочках
+    // то они тоже окажутся в парамсах!! что очень замечательно
+    // в нашем случае мы можем забрать и сервер айди и айди канала
+    params: {
+
+        serverId: string;
+        channelId: string
+    }
+}
+
+const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
+
+    const profile = await currentProfile()
+
+    if (!profile) return redirectToSignIn()
+
+    const channel = await db.channel.findUnique({ // находим канал
+        where: {
+            serverId: params.serverId,
+            id: params.channelId
+        }
+    })
+
+    const member = await db.member.findFirst({
+        where: {
+            serverId: params.serverId,
+            id: profile.id
+        }
+    })
+
     return (
         <div>ChannelIdPage</div>
     )
--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,3 +1,7 @@
+import { currentProfile } from '@/lib/current-profile'
+import { db } from '@/lib/db'
+import { redirectToSignIn } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
 interface ServerPageProps {
@@ -7,9 +11,39 @@ interface ServerPageProps {
 }
 
 const ServerPage = async ({ params }: ServerPageProps) => {
-    return (
-        <div>ServerIdPage</div>
-    )
+
+    const profile = await currentProfile()
+
+    if (!profile) return redirectToSignIn()
+
+    const server = await db.server.findUnique({
+        where: {
+            id: params.serverId,
+            members: {
+                some: { // мы должны чекнуть, что мы есть на этом серваке (хотя бы 1 юзер этого сервера - это мы)
+                    profileId: profile.id
+                }
+            }
+        },
+        include: {
+            Channel: {
+                where: {
+                    name: 'general'
+                },
+                orderBy: {
+                    created_at: 'asc'
+                }
+            }
+
+        }
+
+    })
+
+    const initialChannel = server?.Channel[0] // и забираем из списка первый элемент (general стоит первым, т.к. первый создается) и не может удалиться
+
+
+
+    return redirect(`/servers/${params.serverId}/channels/${initialChannel?.id}`)
 }
 
 export default ServerPage
